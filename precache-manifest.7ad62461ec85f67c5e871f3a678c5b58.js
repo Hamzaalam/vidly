81d3151243d236ee6d781639a59efc75baabaa68
Hamza Alam <smhamza124@gmail.com>
2020-05-05 22:11:45 +0500

    Updates

---
+++ precache-manifest.7ad62461ec85f67c5e871f3a678c5b58.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24ebe822181d54624798391b6d3fc895",
+    "url": "/vidly/index.html"
+  },
+  {
+    "revision": "e4de17d5fbd80c521108",
+    "url": "/vidly/static/css/2.78fe7d83.chunk.css"
+  },
+  {
+    "revision": "11ca844d25ba11be5583",
+    "url": "/vidly/static/css/main.07c7bab9.chunk.css"
+  },
+  {
+    "revision": "e4de17d5fbd80c521108",
+    "url": "/vidly/static/js/2.2f145fc0.chunk.js"
+  },
+  {
+    "revision": "11ca844d25ba11be5583",
+    "url": "/vidly/static/js/main.c8a63792.chunk.js"
+  },
+  {
+    "revision": "fe23e995b9fc200a9f9c",
+    "url": "/vidly/static/js/runtime~main.287d4b85.js"
+  },
+  {
+    "revision": "060ea8dd8446c15159ed6ec217ae29b9",
+    "url": "/vidly/static/media/fa-brands-400.060ea8dd.svg"
+  },
+  {
+    "revision": "4f786efdf6328877ec2448bf265bcc8f",
+    "url": "/vidly/static/media/fa-brands-400.4f786efd.woff2"
+  },
+  {
+    "revision": "72f5c754189a296467db59e26e2b4671",
+    "url": "/vidly/static/media/fa-brands-400.72f5c754.woff"
+  },
+  {
+    "revision": "75dae2b62813bc641a9b6a2fc041e4de",
+    "url": "/vidly/static/media/fa-brands-400.75dae2b6.ttf"
+  },
+  {
+    "revision": "e33a1a54eb0a1e560362fcf5335ac26c",
+    "url": "/vidly/static/media/fa-brands-400.e33a1a54.eot"
+  },
+  {
+    "revision": "0234a585809be071bd68db999f27f829",
+    "url": "/vidly/static/media/fa-regular-400.0234a585.svg"
+  },
+  {
+    "revision": "76d069c84f54a63d1bf9e29c76fe65e6",
+    "url": "/vidly/static/media/fa-regular-400.76d069c8.ttf"
+  },
+  {
+    "revision": "d90e1b03a9168c0d775a52098423adfb",
+    "url": "/vidly/static/media/fa-regular-400.d90e1b03.woff2"
+  },
+  {
+    "revision": "d98ffd73c6b104b42bfe69c987a72924",
+    "url": "/vidly/static/media/fa-regular-400.d98ffd73.eot"
+  },
+  {
+    "revision": "f9695ea8029b2975dea6753bf6f1bf1c",
+    "url": "/vidly/static/media/fa-regular-400.f9695ea8.woff"
+  },
+  {
+    "revision": "55ce8d0ca92adb7c283d6043d0edf46f",
+    "url": "/vidly/static/media/fa-solid-900.55ce8d0c.woff"
+  },
+  {
+    "revision": "60ce8cf4dd9fe177abdfeda21e20798e",
+    "url": "/vidly/static/media/fa-solid-900.60ce8cf4.woff2"
+  },
+  {
+    "revision": "d42cc8bb088b9bbcb51b1db2d9210976",
+    "url": "/vidly/static/media/fa-solid-900.d42cc8bb.svg"
+  },
+  {
+    "revision": "e9809ab6a5a34dc6b7966de82265d6bd",
+    "url": "/vidly/static/media/fa-solid-900.e9809ab6.ttf"
+  },
+  {
+    "revision": "f9b8a1aa417e318d5be775e14ee3c475",
+    "url": "/vidly/static/media/fa-solid-900.f9b8a1aa.eot"
+  }
+]);
